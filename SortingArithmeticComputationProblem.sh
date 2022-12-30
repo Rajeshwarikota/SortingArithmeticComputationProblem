7f2da65e8430f402f2cab6c5b8b1da8c0d5caf7d
Rajeshwarikota <kotarajeswari003.com>
2022-12-30 23:01:56 +0530

    Added: UC7-dictionary array

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -1,4 +1,11 @@
-for i in $( echo "26 12  6 6 " | tr ' ' '\n' | sort -r )
-do
-echo $i
-done
+declare -A Result
+
+read -p "Enter first number a: " a
+read -p "Enter second number b: " b
+read -p "Enter third number c: " c
+Result[val1]="$(($a+$b*$c))"
+Result[val2]="$(($a*$b+$c))"
+Result[val3]="$(($c+$a/$b))"
+Result[val4]="$(($a%$b+$c))"
+
+echo "Results of every computation: "${compute[@]}
