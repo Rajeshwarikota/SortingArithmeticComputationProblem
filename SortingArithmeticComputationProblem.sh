d1327547156f03884fcfb679ab72b0db47bff3a5
Rajeshwarikota <kotarajeswari003.com>
2022-12-30 22:05:58 +0530

    Added: UC6-dictionarycomputation

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -1,5 +1,6 @@
-a=6
-b=2
-c=6
-result=$((a % b + c))
-echo " result = $result "
+declare -A Results
+Results[a+b*c]="26"
+Results[a*b+c]="12"
+Results[c+a/b]="6"
+Results[a%b+c]="6"
+echo "All Computation Results " ${Results[@]}
